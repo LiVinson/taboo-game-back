afd463d5b404e37f55bc2a8f2852bd5ecd8688aa
Lisa Vinson <Lrv0828@gmail.com>
2020-09-21 19:22:48 -0400

    created initial menu options with structures of functions for each option

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const firebase = require('firebase/app')
 require('firebase/firestore')
 require('firebase/analytics')
 
-const fbConfig = require('./fbConfig')
+const fbConfig = require('./config/fbConfig')
 // Initialize Firebase
 firebase.initializeApp(fbConfig)
 
@@ -20,7 +20,9 @@ const mainMenu = () => {
 			{
 				type: 'input',
 				name: 'environmentConfirm',
-				message: `PLEASE READ: All changes made will impact the ${process.env.NODE_ENV.toUpperCase()} database. To show you understand and intend to update the ${process.env.NODE_ENV.toUpperCase()} database, please type '${process.env.NODE_ENV}' and press enter.`,
+				message: `PLEASE READ: All changes made will impact the ${process.env.NODE_ENV.toUpperCase()} database. To show you understand and intend to update the ${process.env.NODE_ENV.toUpperCase()} database, please type '${
+					process.env.NODE_ENV
+				}' and press enter.`,
 				validate: (input) => {
 					if (input.length === 0) {
 						return "Type the environment name to proceed, or press 'Ctrl + C' to end the script"
@@ -31,13 +33,103 @@ const mainMenu = () => {
 					}
 				},
 			},
+			{
+				type: 'rawlist',
+				name: 'menuAction',
+				message: `What would you like to do?`,
+				choices: [
+					{
+						name: 'Import new cards from a file',
+						value: 'newFile',
+					},
+					{
+						name: 'Update existing cards in the deck',
+						value: 'update',
+					},
+					{
+						name: 'Remove a card from the deck',
+						value: 'removeCard',
+					},
+					{
+						name: 'Delete the entire deck',
+						value: 'deleteDeck',
+					},
+					{
+						name: 'Print list of existing cards',
+						value: 'logExisting',
+					},
+					{
+						name: 'Print list of card suggestions',
+						value: 'logSuggestions',
+					},
+					{
+						name: 'Quit',
+						value: 'quit',
+					},
+				],
+			},
 		])
 		.then((response) => {
 			console.log('success')
+			console.log(response)
+			switch (response.menuAction) {
+				case 'newFile':
+					importCardsFromFile()
+					return
+				case 'update':
+					updateExistingCard()
+					return
+				case 'removeCard':
+					removeCardFromDeck()
+					return
+				case 'deleteDeck':
+					deleteDeck()
+					return
+				case 'logExisting':
+					logExistingCards()
+					return
+				case 'logSuggestions':
+					logCardSuggestions()
+					return
+				case 'quit':
+					quit()
+					return
+				default:
+					console.log('something went wrong')
+					return
+			}
 		})
-		.catch((errpr) => {
+		.catch((error) => {
 			console.log(error.message)
 		})
+}
+
+const importCardsFromFile = () => {
+	console.log('import cards from file')
+}
+
+const updateExistingCard = () => {
+	console.log('update existing card')
+}
+
+const removeCardFromDeck = () => {
+	console.log('remove from deck')
+}
+
+const deleteDeck = () => {
+	console.log('delete deck')
+}
+
+const logExistingCards = () => {
+	console.log('log existing cards')
+}
+
+const logCardSuggestions = () => {
+	console.log('log card suggestions')
+}
+
+const quit = () => {
+	console.log('quit')
 }
 
 mainMenu(process.env.NODE_ENV)
